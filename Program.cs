d6ba4dd59979ed5a31e9505b961cb3dff532fc8a
Billy Mathison <wlmathison@gmail.com>
2019-05-28 15:08:36 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace randalls_car_lot_in_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# randalls-car-lot-in-csharp
